ad85c0e6c5bd4cb73000e4f7d7a5247789b7e179
blancaag <blanca@alonsogonzalo.com>
2017-09-09 12:27:43 +0200

    adding functions

--- nbs/libraries.py
+++ nbs/libraries.py
@@ -1,7 +1,6 @@
 import os
 from os import listdir
 from os.path import isfile, join
-# onlyfiles = [f for f in listdir(mypath) if isfile(join(mypath, f))]
 import sys
 import psutil
 import time
@@ -28,7 +27,7 @@ plt.style.use('ggplot')
 
 # import seaborn as sns
 # sns.set_style("whitegrid", {'axes.grid' : False})
-# import multiprocessing
+import multiprocessing
 
 import matplotlib.animation as mpl_animation
 matplotlib.rc('animation', html='html5')
@@ -41,9 +40,7 @@ from scipy.misc.pilutil import imread, imresize
 
 import keras
 from keras import backend as K
-# from keras.datasets import mnist
 from keras import utils
-# from keras.preprocessing import image, sequence
 from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
 from keras.models import *
 from keras.layers import *
@@ -52,12 +49,20 @@ from keras.optimizers import *
 from keras.losses import *
 from keras.utils.np_utils import to_categorical
 
-#import metrics
-#from metrics import * 
+# data preprocessing functions
+from keras.applications.imagenet_utils import preprocess_input 
+from keras.applications.inception_v3 import preprocess_input as preprocess_input_iv3
+from keras.applications.xception import preprocess_input as preprocess_input_x
+from keras.applications.mobilenet import preprocess_input as preprocess_input_mn
+
+from tensorflow.python.client import device_lib
+
+import metrics
+from metrics import * 
 
 from sklearn.model_selection import StratifiedKFold
 
-from tensorflow.python.client import device_lib
+## General utils:
 
 def get_available_gpus():
     local_device_protos = device_lib.list_local_devices()
@@ -72,7 +77,7 @@ def silence():
     return back
 def speak(back): sys.stdout = back
 
-def upscale(im_arr, new_size=(150, 150)):
+def resize(im_arr, new_size=(150, 150)):
     from PIL import Image
     im = Image.fromarray(np.uint8(im_arr))
     im = im.resize(new_size, Image.NEAREST)
@@ -83,10 +88,20 @@ def rescale_normalize(im, mean=0.5, std=0.5):
     im = (im - mean) / std
     return im
 
-
-def load_data(split=0.8, path='../data/original/sushi_or_sandwich/', target_size=(256, 256)):
+# to do: optimize the loading functions
+def load_data(split=0.8, path='../data/sushi_or_sandwich/', target_size=(256, 256), pil=False):
     
-    flabs = [i for i in listdir(path) if not isfile(join(path, i))]
+    cd_folder = '../output/compressed_data'
+    if os.path.exists( cd_folder):
+        
+        x_train = load_array(join(cd_folder, 'x_train'))
+        y_train = load_array(join(cd_folder, 'y_train'))
+        x_test = load_array(join(cd_folder, 'x_test'))
+        y_test = load_array(join(cd_folder, 'y_test'))
+    
+        return x_train, y_train, x_test, y_test, None, None
+    
+    flabs = (join(path, i) for i in listdir(path) if not isfile(join(path, i)))
 
     d_lab = {'sandwich': 0, 'sushi': 1}
 
@@ -94,17 +109,57 @@ def load_data(split=0.8, path='../data/original/sushi_or_sandwich/', target_size
     labs = np.empty((0,))
 
     for i in flabs:
-        ims_gen = (j for j in listdir(path+i) if isfile(join(path+i, j)))
-        for k in ims_gen:
-            im = plt.imread(join(path + i + '/', k))
-            im = cv2.resize(im, target_size).astype(np.float32)
-            im = exposure.rescale_intensity(im)
+        ims_gen = (join(i, j) for j in listdir(i) if isfile(join(i, j)))
+        
+        for j in ims_gen:
+            if pil: im = img_to_array(load_img(j, target_size=target_size))
+            else:
+                im = cv2.imread(j).astype(np.float32)
+                im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB) # b,g,r = cv2.split(im); im = cv2.merge([r,g,b]) 
+                im = cv2.resize(im, target_size)
+
             ims = np.append(ims, im.reshape((1,) + im.shape), axis=0)
-            labs = np.append(labs, (d_lab[i], ), axis=0)
+            labs = np.append(labs, (d_lab[i.split(path)[1]], ), axis=0)
+    
+    return preprocess_data(ims, labs, cd_folder, split)
 
-    return preprocess_data(ims, labs, split)
+def load_data_extended(split=0.8, path='../data/sushi_or_sandwich/', target_size=(256, 256), pil=False):
+    
+    cd_folder = '../output/compressed_data_extended'
+    if os.path.exists( cd_folder):
+        
+        x_train = load_array(join(cd_folder, 'x_train'))
+        y_train = load_array(join(cd_folder, 'y_train'))
+        x_test = load_array(join(cd_folder, 'x_test'))
+        y_test = load_array(join(cd_folder, 'y_test'))
+        
+        return x_train, y_train, x_test, y_test, None, None
+    
+    fsplit = [join(path, i) for i in listdir(path) if not isfile(join(path, i))]
 
-def preprocess_data(ims, labs, split=0.8):
+    d_lab = {'sandwich': 0, 'sushi': 1}
+    ims = np.empty(((0,) + target_size + (3,)))
+    labs = np.empty((0,))
+
+    for i in fsplit:
+        flabs = [join(i, j) for j in listdir(i) if not isfile(join(i, j))]    
+
+        for j in flabs:
+            ims_gen = (join(j, k) for k in listdir(j) if isfile(join(j, k)))
+
+            for k in ims_gen:
+                if pil: im = img_to_array(load_img(k, target_size=target_size))
+                else:
+                    im = cv2.imread(k).astype(np.float32)
+                    im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                    im = cv2.resize(im, target_size)
+
+                ims = np.append(ims, im.reshape((1,) + im.shape), axis=0)
+                labs = np.append(labs, (d_lab[j.split(i)[1].split('/')[1]], ), axis=0)
+                
+    return preprocess_data(ims, labs, cd_folder, split)
+
+def preprocess_data(ims, labs, cd_folder, split=0.8):
     
     # 1. concatenation and shuffling
 
@@ -131,33 +186,50 @@ def preprocess_data(ims, labs, split=0.8):
 
     x_test = x[split_ix:,:,:,:]
     y_test = y[split_ix:]
+    
+    if not os.path.exists(cd_folder): 
+        os.mkdir(cd_folder)
 
-    sys.stdout.flush()
+        save_array(join(cd_folder, 'x_train'), x_train)
+        save_array(join(cd_folder, 'y_train'), y_train)
+        save_array(join(cd_folder, 'x_test'), x_test)
+        save_array(join(cd_folder, 'y_test'), y_test)
     
     return x_train, y_train, x_test, y_test, mean_set, std_set
 
-def take_sample(path, target_size=(512, 512)):
-    ims_gen = (i for i in listdir(path) if isfile(join(path, i)))
+def take_sample(path, target_size=(512, 512), pil=False):
+    ims_gen = (join(path, i) for i in listdir(path) if isfile(join(path, i)))
     for _, i in enumerate(ims_gen):
         if _ == 0:
-            im = plt.imread(join(path, i))
-#             b,g,r = cv2.split(im)           
-#             im = cv2.merge([r,g,b]) 
-            im = cv2.resize(im, target_size).astype(np.float32)
-            im = exposure.rescale_intensity(im)
-            plt.imshow(im)
-            return im
-        else: break
+            if pil: im = img_to_array(load_img(i, target_size=target_size))
+            else:
+                im = cv2.imread(i).astype(np.float32)
+                im = cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
+                im = cv2.resize(im, target_size)
+                plt.axis('off')
+                plt.tight_layout()
+                plt.imshow(im.astype(np.uint8))
+                return im
             
-def plot_images(data, ncols=3, beautiful=False):
+def plot(im):
+    if im.ndim == 4: im = im[0]
+    plt.axis('off')
+    plt.tight_layout()
+    plt.imshow(im.astype(np.uint8))
+            
+def plot_ims(data, ncols=3, beautiful=False):
+
+    if data.shape[0] % ncols != 0:
+        raise ValueError('Expecting data to contain a number of images multiple of 3 but got an array with shape:', data.shape[0])
+    
     ncols = ncols 
     nrows = data.shape[0] // ncols
+    
     fig, axes = plt.subplots(nrows=nrows, ncols=ncols)
     fig.set_size_inches(12, nrows*5)
     fig.tight_layout()
     for i, ax in enumerate(axes.flat):
-        if beautiful: im = ax.imshow(data[i]*255)
-        else: im = ax.imshow(data[i])
+        im = ax.imshow(data[i].astype(np.uint8))
         ax.set_axis_off()
         ax.title.set_visible(False)
         ax.xaxis.set_ticks([])
@@ -166,7 +238,9 @@ def plot_images(data, ncols=3, beautiful=False):
             spine.set_visible(False)
     plt.subplots_adjust(left=0, hspace=0, wspace=0)
     plt.show()
-    
+
+## Modelling utils
+
 def plot_hist(h, key='loss'):
     fig = plt.figure(figsize=(10, 3))
     plt.plot(h.history[key])  
@@ -183,36 +257,106 @@ def evaluate(model, x_test, y_test):
     for i,j in zip(model.metrics_names, range(len(metrics))):
             print("Test %s: %.2f%%" % (i, metrics[j] * 100))
     
-    
-# defining the augmented data generators:
+## Data augmentation utils:
 
-def aug_data_generators():
+# cropping functions
+
+def center_crop(x, center_crop_size = (360, 360), **kwargs):
+    centerw, centerh = x.shape[1]//2, x.shape[2]//2
+    halfw, halfh = center_crop_size[0]//2, center_crop_size[1]//2
+    return x[:, centerw-halfw:centerw+halfw,centerh-halfh:centerh+halfh]
+
+def random_crop(x, random_crop_size = (360, 360), sync_seed=None, **kwargs):
+    np.random.seed(sync_seed)
+    w, h = x.shape[1], x.shape[2]
+    rangew = (w - random_crop_size[0]) // 2
+    rangeh = (h - random_crop_size[1]) // 2
+    offsetw = 0 if rangew == 0 else np.random.randint(rangew)
+    offseth = 0 if rangeh == 0 else np.random.randint(rangeh)
+    return x[:, offsetw:offsetw+random_crop_size[0], offseth:offseth+random_crop_size[1]]
+
+# custom preprocessing function
+
+def preprocess_input_function(x, model, crop=None):
+    """Preprocesses a tensor encoding a batch of images.
+    # Arguments
+        x: input Numpy tensor, 4D.
+        model: pretrained model to be used
+        crop: ['centered', 'random', None].
+    # Returns
+        Preprocessed tensor.
+    """
+    
+    if model in ['inception_v3', 'xception', 'mobilenet']: 
+        x /= 255.
+        x -= 0.5
+        x *= 2.
+        
+    elif model in ['densenet_121', 'densenet_161', 'densenet_169']:
+        # Subtract mean pixel and multiple by scaling constant 
+        # Reference: https://github.com/shicai/DenseNet-Caffe
+        x[:,:,0] = (x[:,:,0] - 103.94) * 0.017
+        x[:,:,1] = (x[:,:,1] - 116.78) * 0.017
+        x[:,:,2] = (x[:,:,2] - 123.68) * 0.017
+        
+    elif model in ['resnet_50', 'vgg_16', 'vgg_19']:
+        # 'RGB'->'BGR'
+        x = x[..., ::-1]
+        # Zero-center by mean pixel
+        x[..., 0] -= 103.939
+        x[..., 1] -= 116.779
+        x[..., 2] -= 123.68
+
+    if crop=='centered': center_crop(x)
+    if crop=='random': random_crop(x)
+         
+    return x
+
+# defining the augmented data generators
+
+def aug_data_generators(model, n_gen):
+    
+    if model in ['iv3', 'xcep', 'mobilenet']: preprocess_input_function = preprocess_input_iv3
+    else: preprocess_input_function = preprocess_input
+    
+    fill_mode = ['constant', 'nearest', 'reflect', 'wrap']
     
     ad_gens = []
     count = 0
-    for i in range(0, 10, 3):
-        for j in range(0, 10, 2):
-            for k in range(0, 6, 1): 
-                count += 1
-                exec("""ad_gen_{} = ImageDataGenerator(
-                        horizontal_flip=True,
-                        vertical_flip=True,
-                        rotation_range={},             # 
-                        width_shift_range={}/10,       # 
-                        height_shift_range={}/10,      # 
-                        shear_range={}/10,             # 
-                        zoom_range={}/10,              # 
-                        fill_mode='nearest')
-                """.format(count, i, j, j, k, k))
-                exec('ad_gens.append(ad_gen_{})'.format(count))
-    print("Total number of generators: ", len(ad_gens))
+    for i in range(0, 180, 60): 
+        for j in range(0, 8, 3):
+            for k in range(0, 10, 3): 
+                for l in range(0, 50, 10):
+                    for m in [0, 1]:
+                        for n in fill_mode:
+                            count += 1
+                            exec("""ad_gen_{} = ImageDataGenerator(
+                                    rotation_range={},             # i
+                                    width_shift_range={}/10,       # j
+                                    height_shift_range={}/10,      # j
+                                    shear_range={}/10,             # j
+                                    zoom_range={}/10,              # k
+                                    channel_shift_range={},        # l
+                                    zca_whitening={},              # m
+                                    zca_epsilon=1e-06, 
+                                    fill_mode={},                  # n
+                                    cval=0,                        
+                                    horizontal_flip=1, 
+                                    vertical_flip=1, 
+                                    preprocessing_function=preprocess_input_function)
+                            """.format(count, i, j, j, j, k, l, m, 'n'))
+                            exec('ad_gens.append(ad_gen_{})'.format(count))
+    print("Total number of available aug. data generators: ", len(ad_gens))
     
-    return ad_gens
+    shuffle_ix = list(np.random.randint(0, len(ad_gens), n_gen))
+    ad_gens_g = (ad_gens[i] for i in shuffle_ix)
+    print("Selected number of aug. data generators: ", len(shuffle_ix))
+    
+    return ad_gens_g
 
+# generates and saves a sample of the augmented data generators     
 
-# generates and saves a sample of the augmented data generators:     
-
-def save_aug_data_sample(ad_gens, n_plot=6, set_return=False):
+def save_aug_data_sample(ad_gens, input_shape, data_path, n_plot=6, set_return=False):
     _ = silence() # filters the stdout from 'flow_from_directory'
 
     sample_folder = 'aug_data_sample'
@@ -220,30 +364,29 @@ def save_aug_data_sample(ad_gens, n_plot=6, set_return=False):
     
     sample = np.empty(((0,) + input_shape))
     for gen in ad_gens:
-        for i, j in gen.flow_from_directory('../../data/sushi_or_sandwich/', 
-                                             batch_size=1, seed=0,
-                                             save_to_dir=sample_folder, save_prefix='ex'):
+        for i, j in gen.flow_from_directory(data_path, 
+                                            batch_size=1, seed=0,
+                                            save_to_dir=sample_folder, save_prefix='ex'):
 
             sample = np.append(sample, i, axis=0); 
             sample_lab = j
             break
     speak(_)
     
-    s_plot = list(np.random.randint(0, ad_sample.shape[0], n_plot))
-    plot_images(s_plot)
+    s_plot_ix = list(np.random.randint(0, sample.shape[0], n_plot))
+    plot_images(sample[s_plot_ix])
     
     if set_return: 
         return sample, sample_lab
-
         
 # pre-calculates without-top model output ('features')
 
-def generate_aug_data_features(model, ad_gens, data, output_folder, batch_size=16, iters = 1):
+def generate_aug_data_features(base_model, ad_gens, data, output_folder, batch_size=16, iters = 1, verbose=False):
     
-    x_adf_train = np.empty(((0,) + model.output_shape[1:]))
+    x_adf_train = np.empty(((0,) + base_model.output_shape[1:]))
     y_adf_train = np.empty((0,))
 
-    x_adf_test = np.empty(((0,) + model.output_shape[1:]))
+    x_adf_test = np.empty(((0,) + base_model.output_shape[1:]))
     y_adf_test = np.empty((0,))
 
     for i in ad_gens:
@@ -251,25 +394,29 @@ def generate_aug_data_features(model, ad_gens, data, output_folder, batch_size=1
         adg_train = i.flow(data['x_train'], data['y_train'], batch_size=batch_size, shuffle=False)
         adg_test = i.flow(data['x_test'], data['y_test'], batch_size=batch_size, shuffle=False)
 
-        # print(adg_train.n//adg_train.batch_size, adg_test.n//adg_test.batch_size)
+        #print(adg_train.n//adg_train.batch_size, adg_test.n//adg_test.batch_size)
 
-        x_ad_feat_train = model.predict_generator(adg_train, iters*adg_train.n//adg_train.batch_size, verbose=0)
+        x_ad_feat_train = base_model.predict_generator(adg_train, iters*adg_train.n//adg_train.batch_size, verbose=0)
         y_ad_feat_train = np.concatenate([data['y_train']]*iters)
-        x_ad_feat_test = model.predict_generator(adg_test, iters*adg_test.n//adg_test.batch_size, verbose=0)
+        x_ad_feat_test = base_model.predict_generator(adg_test, iters*adg_test.n//adg_test.batch_size, verbose=0)
         y_ad_feat_test = np.concatenate([data['y_test']]*iters)
- 
+         
+        #print (x_ad_feat_train.shape, y_ad_feat_train.shape, x_ad_feat_test.shape, y_ad_feat_test.shape)
+            
         x_adf_train = np.append(x_adf_train, x_ad_feat_train, axis=0)
         y_adf_train = np.append(y_adf_train, y_ad_feat_train, axis=0)
         x_adf_test = np.append(x_adf_test, x_ad_feat_test, axis=0)
         y_adf_test = np.append(y_adf_test, y_ad_feat_test, axis=0)
+        
+        if verbose: print("Generator ", i)
 
     # saving
-    save_array(output_folder + '/x_ad_feat_train', x_ad_feat_train)
-    save_array(output_folder + '/y_ad_feat_train', y_ad_feat_train)
-    save_array(output_folder + '/x_ad_feat_test', x_ad_feat_test)
-    save_array(output_folder + '/y_ad_feat_test', y_ad_feat_test)
+    save_array(join(output_folder, 'x_ad_feat_train'), x_ad_feat_train)
+    save_array(join(output_folder, 'y_ad_feat_train'), y_ad_feat_train)
+    save_array(join(output_folder, 'x_ad_feat_test'), x_ad_feat_test)
+    save_array(join(output_folder, 'y_ad_feat_test'), y_ad_feat_test)
 
-    print(x_adf_train.shape, y_adf_train.shape, x_adf_test.shape, y_adf_test.shape)
+    return x_adf_train, y_adf_train, x_adf_test, y_adf_test
     
 # rm train_2225.jpg
 # rm train_2276.jpg 
