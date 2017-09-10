ec0f2a67771c465a3dfa6addfb30511b930385fa
blancaag <blanca@alonsogonzalo.com>
2017-09-10 12:45:25 +0200

    ad .

---
+++ scripts/validation.py
@@ -0,0 +1,19 @@
+"""
+import libraries
+from libraries import *
+
+import utils
+from utils import *
+
+%matplotlib inline
+
+
+# checking
+print(get_available_gpus())
+print(psutil.virtual_memory())
+print(keras.__version__)
+
+print(test)
+"""
+
+print(validation test script...)
